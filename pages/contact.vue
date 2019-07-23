5aae6498c316f0dc3d8141dd4ce2ad35236d1783
Razelle McCarrick <razelle@satyaorthopedic.com>
2019-07-23 17:49:32 -0400

    still trying to fix contact form

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="contact page">
-    <form name="contact" netlify>
+    <form name="contact" method="POST" netlify>
       <label>Your Name:</label>
       <input type="text" name="name" />
 
