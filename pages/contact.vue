e238b35e5ae1a9ee130d6c9991eb0e19ffd5767a
Razelle McCarrick <razelle@satyaorthopedic.com>
2019-07-23 18:47:11 -0400

    form fix

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="contact page">
-    <h1 class="title">Contact</h1>
-    <div class="content">
-      <form name="contact" data-netlify-honeypot="bot-field" method="post" netlify>
-        <input type="hidden" name="form-name" value="contact" />
-        <label class="form-label" for="name">Name:</label>
-        <input class="form-field" name="name" id="name" />
-        <label class="form-label" for="email">Email:</label>
-        <input class="form-field" name="email" id="email" />
-        <label class="form-label" for="message">Message:</label>
-        <textarea class="form-field" name="message" id="message"></textarea>
-        <input class="form-button" type="submit" value="Send message" />
-      </form>
-    </div>
+    <form name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
+      <input type="hidden" name="form-name" value="contact" />
+      <label>Your Name:</label>
+      <input type="text" name="name" />
+
+      <label>Your Email:</label>
+      <input type="email" name="email" />
+
+      <label>Message:</label>
+      <textarea name="message"></textarea>
+
+      <input type="submit" value="Send" name="submit" />
+    </form>
   </div>
 </template>
 
