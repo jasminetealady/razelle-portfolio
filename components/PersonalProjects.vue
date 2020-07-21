3bb2d0da165105a8338a48b8b37edecd62fec0bf
Razelle McCarrick <razelle@satyaorthopedic.com>
2020-07-21 19:36:11 -0400

    added gatsby project

--- components/PersonalProjects.vue
+++ components/PersonalProjects.vue
@@ -3,16 +3,9 @@
     <h1>projects</h1>
     <div class="wrapper">
       <Project
-        title="shahid buttar"
-        text="A concept redesign of Shahid Buttar's campaign website."
-        :projectUrl="'https://shahid-buttar.netlify.app'"
-      />
-      <Project
-        title="voter dash"
-        text="
-          A voter information app with a custom API that provides information on voter registration deadlines and the website to check voter registration in the user's state.
-        "
-        :projectUrl="'https://voter-info.netlify.app'"
+        title="gatsby portfolio site"
+        text="A Gatsby recreation of razellemccarrick.com using WordPress as a back end with GraphQL."
+        :projectUrl="'https://razelle.netlify.app'"
       />
       <Project
         title="prismic blog"
@@ -23,6 +16,18 @@
         title="storyblok site demo"
         text="A demo site that pulls from the Storyblok CMS."
         :projectUrl="'https://storyblok-demo.herokuapp.com/'"
+      />
+      <Project
+        title="shahid buttar"
+        text="A concept redesign of Shahid Buttar's campaign website."
+        :projectUrl="'https://shahid-buttar.netlify.app'"
+      />
+      <Project
+        title="voter dash"
+        text="
+          A voter information app with a custom API that provides information on voter registration deadlines and the website to check voter registration in the user's state.
+        "
+        :projectUrl="'https://voter-info.netlify.app'"
       />
     </div>
   </section>
